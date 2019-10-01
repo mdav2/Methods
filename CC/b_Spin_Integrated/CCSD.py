40b1cd193c02bca04cf508e2c6a4e21798008027
Gustavo Aroeira <aroeira@Mac-mini-3.local>
2019-09-30 20:49:04 -0400

    Trying to fix singles

--- CC/b_Spin_Integrated/CCSD.py
+++ CC/b_Spin_Integrated/CCSD.py
@@ -19,129 +19,96 @@ def cc_energy(T1, T2):
     E = np.einsum('abij,ijab->', Vint[v,v,o,o], X)
     return E
 
-def T2_iter(T2, T1):
+def T2_iter(T1, T2):
 
-        tau = T2 + np.einsum('ia,jb->ijab', T1, T1)
-        V = 2*Vint - np.einsum('ijab->ijba', Vint)
-        Te = 0.5*T2 + np.einsum('ia,jb->ijab', T1, T1)
+    tau = T2 + np.einsum('ia,jb->ijab', T1, T1)
+    V = 2*Vint - np.einsum('abij->baij', Vint)
+    Te = 0.5*T2 + np.einsum('ia,jb->ijab', T1, T1)
+    
+    T2new = copy.deepcopy(Vint[o,o,v,v])
+    
+    T2new += np.einsum('uvij,ijpg->uvpg', Vint[o,o,o,o], tau)
+    
+    T2new += np.einsum('abpg,uvab->uvpg', Vint[v,v,v,v], tau)
+    
+    T2new += np.einsum('abij,ijpg,uvab->uvpg', Vint[v,v,o,o], tau, tau)
+    
+    T2new = 0.5*T2new
+    
+    T2new += np.einsum('uapg,va->uvpg', Vint[o,v,v,v], T1)
+    
+    T2new -= np.einsum('uvpi,ig->uvpg', Vint[o,o,v,o], T1)
+    
+    T2new += np.einsum('auip,viga->uvpg', V[v,o,o,v], T2)
+    
+    T2new -= np.einsum('uaig,ivpa->uvpg', Vint[o,v,o,v], tau)
+    
+    T2new -= np.einsum('auip,viag->uvpg', Vint[v,o,o,v], tau)
+    
+    X = T2 - np.einsum('uipa->uiap',tau)
+    
+    T2new += np.einsum('abij,vjgb,uipa->uvpg', V[v,v,o,o], T2, X)
+    
+    T2new -= np.einsum('abij,ijgb,uvpa->uvpg', V[v,v,o,o], tau, T2)
+    
+    T2new -= np.einsum('abij,vjab,uipg->uvpg', V[v,v,o,o], tau, T2)
+    
+    T2new += np.einsum('abij,vjbg,uiap->uvpg', Vint[v,v,o,o], T2, Te)
+    
+    T2new += np.einsum('abij,ujag,vibp->uvpg', Vint[v,v,o,o], T2, Te)
+    
+    #X = np.einsum('ivpg,ja->ivjpga', T2, T1) + np.einsum('jvag,ip->ivjpga', T2, T1)
+    X = np.einsum('ivpg,ja->ivjpga', T2, T1) + np.einsum('jvag,ip->jviagp', T2, T1)
+    
+    #Y = np.einsum('vjag,ip->vjiagp', T2, T1) + np.einsum('viap,jg->vjiagp', T2, T1) + np.einsum('ijpg,va->vjiagp', tau, T1)
+    Y = np.einsum('vjag,ip->vjiagp', T2, T1) + np.einsum('viap,jg->vijapg', T2, T1) + np.einsum('ijpg,va->ijvpga', tau, T1)
+    
+    T2new -= np.einsum('uaij,ivjpga,uaij,vjiagp->uvpg', V[o,v,o,o], X, Vint[o,v,o,o], Y)
+    
+    X = np.einsum('uvag,ib->uviagb', T2, T1) + np.einsum('ivbg,ua->uviagb', T2, T1)
+    
+    T2new += np.einsum('abpi,uviagb->uvpg', V[v,v,v,o], X)
+    
+    Y = np.einsum('ivgb,ua->ivugba', T2, T1) + np.einsum('iuga,vb->ivugba', T2, T1) + np.einsum('uvab,ig->ivugba', tau, T1)
+    
+    T2new -= np.einsum('abpi,ivugba->uvpg', Vint[v,v,v,o], Y)
+    
+    # Finish it up: Apply the permutation operator and divide by orbital energies [D(ijab)]
+    
+    T2new = T2new + np.einsum('uvpg->vugp',T2new)
+    
+    T2new = np.einsum('uvpg,uvpg->uvpg', T2new, D)
+    
+    res = np.sum(np.abs(T2new - T2))
+    
+    return T2new, res
 
-        T2new = copy.deepcopy(Vint)
-        
-        T2new += np.einsum('uvij,ijpg->uvpg', Vint, tau)
+def T1_iter(T1, T2):
+    # Auxiliar (Intermediate) Arrays 
+    
+    tau = T2 + np.einsum('ia,jb->ijab', T1, T1)
+    V = 2*Vint - np.einsum('abij->baij', Vint)
+    
+    T1new  = -np.einsum('auip,ia->up', V[v,o,o,v], T1)
+    
+    Y = 2*T2 - tau.swapaxes(2,3) 
 
-        T2new += np.einsum('pgab,uvab->uvpg', Vint, tau)
+    X = np.einsum('ijpb,ua->ijupba',T2,T1) + np.einsum('ujab,ip->ijupba',T2,T1) - np.einsum('ujpb,ia->ijupba', Y, T1)
+    
+    T1new += np.einsum('abij,ijupba->up', V[v,v,o,o], X)
+    
+    T1new += np.einsum('auij,ijap->up', V[v,o,o,o], tau)
+    
+    T1new -= np.einsum('abip,iuab->up', V[v,v,o,v], tau)
+    
+    T1new = np.einsum('up,up->up', T1new, d)
+    
+    res = np.sum(np.abs(T1new - T1))
+    
+    return T1new, res
 
-        T2new += np.einsum('ijab,ijpg,uvab->uvpg', Vint, tau, tau)
+    
 
-        T2new = 0.5*T2new
-
-        T2new += np.einsum('pgua,va->uvpg', Vint, T1)
-
-        T2new -= np.einsum('piuv,ig->uvpg', Vint, T1)
-
-        T2new += np.einsum('ipau,viga->uvpg', V, T2)
-
-        T2new -= np.einsum('igua,ivpa->uvpg', Vint, tau)
-
-        T2new -= np.einsum('ipau,viag->uvpg', Vint, tau)
-
-        X = T2 - np.einsum('uipa->uiap',tau)
-
-        T2new += np.einsum('ijab,vjgb,uipa->uvpg', V, T2, X)
-
-        T2new -= np.einsum('ijab,ijgb,uvpa->uvpg', V, tau, T2)
-
-        T2new -= np.einsum('ijab,vjab,uipg->uvpg', V, tau, T2)
-
-        T2new += np.einsum('ijab,vjbg,uiap->uvpg', Vint, T2, Te)
-
-        T2new += np.einsum('ijab,ujag,vibp->uvpg', Vint, T2, Te)
-
-        X = np.einsum('ivpg,ja->ivjpga', T2, T1) + np.einsum('jvag,ip->ivjpga', T2, T1)
-
-        Y = np.einsum('vjag,ip->vjiagp', T2, T1) + np.einsum('viap,jg->vjiagp', T2, T1) + np.einsum('ijpg,va->vjiagp', tau, T1)
-
-        T2new -= np.einsum('ijua,ivjpga,ijua,vjiagp->uvpg', V, X, Vint, Y)
-
-        X = np.einsum('uvag,ib->uviagb', T2, T1) + np.einsum('ivbg,ua->uviagb', T2, T1)
-
-        T2new += np.einsum('piab,uviagb->uvpg', V, X)
-
-        Y = np.einsum('ivgb,ua->ivugba', T2, T1) + np.einsum('iuga,vb->ivugba', T2, T1) + np.einsum('uvab,ig->ivugba', tau, T1)
-
-        T2new -= np.einsum('piab,ivugba->uvpg', Vint, Y)
-
-        # Finish it up: Apply the permutation operator and divide by orbital energies [D(ijab)]
-
-        T2new = T2new + np.einsum('uvpg->vugp',T2new)
-
-        T2new = np.einsum('uvpg,uvpg->uvpg', T2new, D)
-
-        res = np.sum(np.abs(T2new - T2))
-
-        return T2new, res
-
-def b_T2_iter(T2, T1):
-
-        tau = T2 + np.einsum('ia,jb->ijab', T1, T1)
-        V = 2*Vint - np.einsum('abij->baij', Vint)
-        Te = 0.5*T2 + np.einsum('ia,jb->ijab', T1, T1)
-
-        T2new = copy.deepcopy(Vint[o,o,v,v])
-        
-        T2new += np.einsum('uvij,ijpg->uvpg', Vint[o,o,o,o], tau)
-
-        T2new += np.einsum('abpg,uvab->uvpg', Vint[v,v,v,v], tau)
-
-        T2new += np.einsum('abij,ijpg,uvab->uvpg', Vint[v,v,o,o], tau, tau)
-
-        T2new = 0.5*T2new
-
-        T2new += np.einsum('uapg,va->uvpg', Vint[o,v,v,v], T1)
-
-        T2new -= np.einsum('uvpi,ig->uvpg', Vint[o,o,v,o], T1)
-
-        T2new += np.einsum('auip,viga->uvpg', V[v,o,o,v], T2)
-
-        T2new -= np.einsum('uaig,ivpa->uvpg', Vint[o,v,o,v], tau)
-
-        T2new -= np.einsum('auip,viag->uvpg', Vint[v,o,o,v], tau)
-
-        X = T2 - np.einsum('uipa->uiap',tau)
-
-        T2new += np.einsum('abij,vjgb,uipa->uvpg', V[v,v,o,o], T2, X)
-
-        T2new -= np.einsum('abij,ijgb,uvpa->uvpg', V[v,v,o,o], tau, T2)
-
-        T2new -= np.einsum('abij,vjab,uipg->uvpg', V[v,v,o,o], tau, T2)
-
-        T2new += np.einsum('abij,vjbg,uiap->uvpg', Vint[v,v,o,o], T2, Te)
-
-        T2new += np.einsum('abij,ujag,vibp->uvpg', Vint[v,v,o,o], T2, Te)
-
-        X = np.einsum('ivpg,ja->ivjpga', T2, T1) + np.einsum('jvag,ip->ivjpga', T2, T1)
-
-        Y = np.einsum('vjag,ip->vjiagp', T2, T1) + np.einsum('viap,jg->vjiagp', T2, T1) + np.einsum('ijpg,va->vjiagp', tau, T1)
-
-        T2new -= np.einsum('uaij,ivjpga,uaij,vjiagp->uvpg', V[o,v,o,o], X, Vint[o,v,o,o], Y)
-
-        X = np.einsum('uvag,ib->uviagb', T2, T1) + np.einsum('ivbg,ua->uviagb', T2, T1)
-
-        T2new += np.einsum('abpi,uviagb->uvpg', V[v,v,v,o], X)
-
-        Y = np.einsum('ivgb,ua->ivugba', T2, T1) + np.einsum('iuga,vb->ivugba', T2, T1) + np.einsum('uvab,ig->ivugba', tau, T1)
-
-        T2new -= np.einsum('abpi,ivugba->uvpg', Vint[v,v,v,o], Y)
-
-        # Finish it up: Apply the permutation operator and divide by orbital energies [D(ijab)]
-
-        T2new = T2new + np.einsum('uvpg->vugp',T2new)
-
-        T2new = np.einsum('uvpg,uvpg->uvpg', T2new, D)
-
-        res = np.sum(np.abs(T2new - T2))
-
-        return T2new, res
 # Input Geometry    
 
 #H2 = psi4.geometry("""
@@ -170,7 +137,7 @@ water = psi4.geometry("""
 
 # Basis set
 
-basis = 'cc-pvtz'
+basis = '3-21g'
 
 # Psi4 Options
 
@@ -230,19 +197,12 @@ print('\n----------------- RUNNING CCD ------------------')
 
 print('\nBuilding Auxiliar D matrix...')
 t = time.time()
-#D  = np.zeros([nbf, nbf, nbf, nbf])
-#range1 = range(0, ndocc)
-#range2 = range(ndocc, nbf)
-#for i in range1:
-#    for a in range2:
-#        for j in range1:
-#            for b in range2:
-#                D[i,j,a,b] = 1/(eps[i] + eps[j] - eps[a] - eps[b])
-
 D  = np.zeros([ndocc, ndocc, nvir, nvir])
+d  = np.zeros([ndocc, nvir])
 for i,ei in enumerate(eps[o]):
     for j,ej in enumerate(eps[o]):
         for a,ea in enumerate(eps[v]):
+            d[i,a] = 1/(ea - ei)
             for b,eb in enumerate(eps[v]):
                 D[i,j,a,b] = 1/(ei + ej - ea - eb)
 
@@ -259,6 +219,7 @@ E = cc_energy(T1, T2)
 
 print('MP2 Energy: {:<5.10f}     Time required: {:.5f}'.format(E+scf_e, time.time()-t))
 
+r1 = 0
 r2 = 1
 CC_CONV = 6
 CC_MAXITER = 20
@@ -267,21 +228,25 @@ LIM = 10**(-CC_CONV)
 
 ite = 0
 
-while r2 > LIM:
+while r2 > LIM or r1 > LIM:
     ite += 1
     if ite > CC_MAXITER:
         raise NameError("CC Equations did not converge in {} iterations".format(CC_MAXITER))
     Eold = E
     t = time.time()
-    T2,r2 = b_T2_iter(T2, T1)
+    T1N, r1 = T1_iter(T1, T2)
+    T2,r2 = T2_iter(T1, T2)
+    T1 = copy.deepcopy(T1N)
     E = cc_energy(T1, T2)
     dE = E - Eold
     print('-'*50)
     print("Iteration {}".format(ite))
     print("CC Correlation energy: {}".format(E))
     print("Energy change:         {}".format(dE))
+    print("T1 Residue:            {}".format(r1))
     print("T2 Residue:            {}".format(r2))
-    print("Max Amplitude:         {}".format(np.max(T2)))
+    print("Max T1 Amplitude:      {}".format(np.max(T1)))
+    print("Max T2 Amplitude:      {}".format(np.max(T2)))
     print("Time required:         {}".format(time.time() - t))
     print('-'*50)
 
